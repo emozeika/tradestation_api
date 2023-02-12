4ff2ee6a8785be777491f43d553f497195ff96ec
emozeika <mozeikaeric@gmail.com>
2023-02-11 22:21:50 -0500

    cleaned package requirements up; created function for automating authorization

--- client.py
+++ client.py
@@ -1,14 +1,22 @@
 #loading dependencies
 import webbrowser
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
+
+
+from config import CHROME_DRIVER_PATH
+import time
 
 class TradeStationClient(object):
 
     def __init__(
         self, 
         username: str, 
+        password: str,
         client_id: str, 
         client_secret: str, 
         scope: list | str = 'openid', 
@@ -33,6 +41,7 @@ class TradeStationClient(object):
             #instance defined parameters
             'client_id': client_id,
             'username': username,
+            'password':password,
             'client_secret' : client_secret,
             'redirect_uri' : 'http://localhost:8080',
             'scope' : scope,
@@ -76,7 +85,32 @@ class TradeStationClient(object):
     
     def _authorize_auto(self) -> None:
         #TODO: build automatic process for grabbing auth code
-        pass
+        auth_url = self._build_auth_url()
+
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+
+
+        driver = webdriver.Chrome(CHROME_DRIVER_PATH, chrome_options=chrome_options)
+        driver.get(auth_url)
+        time.sleep(1)
+
+        #inputting credentials
+        driver.find_element(By.ID, 'username').send_keys(self.CONFIG_['username'])
+        driver.find_element(By.ID, 'password').send_keys(self.CONFIG_['password'])
+        driver.find_element(By.ID, 'btn-login').click()
+        time.sleep(1)
+
+        #checking if needing to 2FA
+        if 'https://signin.tradestation.com/u/mfa-sms-challenge' in driver.current_url:
+            driver.find_element(By.ID, 'code').send_keys(input('Enter 2FA code from text message: '))
+            #TODO: make line above manual
+        time.sleep(1)
+
+        #grabbing auth_code from current_url
+        auth_code = driver.current_url.split['code='][-1].split['&'][0]
+        print(auth_code)
+        return auth_code
 
     def _get_access_token(self) -> dict:
         '''
